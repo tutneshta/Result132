c16073257bcec487e146d022aaac02a200a40786
tutneshta@yandex.ru <Tut766383000>
2022-10-10 13:50:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Result132
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:\\task13\\Text.txt";
+
+            string text = File.ReadAllText(path);
+
+            string noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            var words = noPunctuationText
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => w.Length >= 3)
+                .GroupBy(w => w)
+                .OrderByDescending(g => g.Count());
+
+            int i = 0;
+
+            foreach (var word in words)
+            {
+                Console.WriteLine("{0}: {1}", word.Count(), word.Key);
+
+                i++;
+
+                if (i == 10)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
